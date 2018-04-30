9e828ab071dae5e8d5e57c6b7094be4feced03f5
Rahul Parkar <rahul.a.parkar@gmail.com>
2018-04-30 22:03:03 +0100

    Add basic header to prove all works

---
+++ src/components/MainMenu/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Image, Menu, Button } from 'semantic-ui-react';
+
+class MainMenu extends React.PureComponent {
+  static propTypes = {
+    authInfo: PropTypes.shape()
+  };
+
+  static defaultProps = {
+    authInfo: null
+  };
+
+  onLoginButtonClick = () => {
+    window.location.href = 'https://login.vatusa.net';
+  };
+
+  renderLoginButton() {
+    return (
+      <Button as="a" primary onClick={this.onLoginButtonClick}>
+        Log in
+      </Button>
+    );
+  }
+
+  render() {
+    return (
+      <Container>
+        <Menu.Item>
+          <Image size="small" />
+        </Menu.Item>
+        <Menu.Item as="a" active>
+          Home
+        </Menu.Item>
+        <Menu.Item as="a">Facilities</Menu.Item>
+        <Menu.Item as="a">Forums</Menu.Item>
+        <Menu.Item as="a">Division Info</Menu.Item>
+        <Menu.Item as="a">Pilot Tools</Menu.Item>
+        <Menu.Item as="a">Join Us</Menu.Item>
+        <Menu.Item as="a">TMU Maps</Menu.Item>
+        <Menu.Item as="a">Support</Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item className="item">{this.renderLoginButton()}</Menu.Item>
+        </Menu.Menu>
+      </Container>
+    );
+  }
+}
+
+export default MainMenu;
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,25 +2,80 @@ import 'styles/global';
 import theme from 'styles/theme';
 
 import React from 'react';
-import PropTypes from 'prop-types';
+
+import {
+  Container,
+  Header,
+  Menu,
+  Segment,
+  Visibility
+} from 'semantic-ui-react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import { ThemeProvider } from 'styled-components';
 import { PageLayout } from './style';
 
-class Layout extends React.Component {
-  static propTypes = {
-    children: PropTypes.element
-  };
+import MainMenu from 'components/MainMenu';
 
-  static defaultProps = {
-    children: <div>Test</div>
-  };
+class Layout extends React.Component {
+  state = {};
+
+  hideFixedMenu = () => this.setState({ visible: false });
+  showFixedMenu = () => this.setState({ visible: true });
+
+  renderFixedMenu() {
+    return (
+      <Menu fixed="top" size="large">
+        <MainMenu />
+      </Menu>
+    );
+  }
 
   render() {
-    const { children } = this.props;
+    const { visible } = this.state;
     return (
       <ThemeProvider theme={theme}>
-        <PageLayout>{children}</PageLayout>
+        <PageLayout>
+          <BrowserRouter>
+            <div>
+              {visible ? this.renderFixedMenu() : null}
+
+              <Visibility
+                onBottomPassed={this.showFixedMenu}
+                onBottomVisible={this.hideFixedMenu}
+                once={false}
+              >
+                <Segment
+                  inverted
+                  textAlign="center"
+                  style={{ minHeight: 300, padding: '0em 0em' }}
+                  vertical
+                >
+                  <Menu inverted borderless stackable size="large">
+                    <MainMenu />
+                  </Menu>
+
+                  <Container text>
+                    <Header
+                      as="h1"
+                      content="VATUSA"
+                      inverted
+                      style={{
+                        fontSize: '4em',
+                        fontWeight: 'normal',
+                        marginBottom: 0,
+                        marginTop: '0.5em'
+                      }}
+                    />
+                  </Container>
+                </Segment>
+              </Visibility>
+              <Switch>
+                <Route exact path="/" />
+              </Switch>
+            </div>
+          </BrowserRouter>
+        </PageLayout>
       </ThemeProvider>
     );
   }
