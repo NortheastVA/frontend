4f3d3283eae6f729e95fe484a640b955a9ca68a3
Rahul Parkar <rahul.a.parkar@gmail.com>
2018-06-02 17:10:44 +0100

    Initial app bar with material bits and bobs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import 'airbnb-browser-shims';
 import { Provider } from 'react-redux';
+import 'babel-polyfill';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import Layout from 'pages/layout';
 
 // import { fetchAuth } from './ducks/auth';
 import configureStore from './store/configureStore';
@@ -9,10 +13,26 @@ import configureStore from './store/configureStore';
 const store = configureStore();
 // store.dispatch(fetchAuth());
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#1565c0',
+    },
+    secondary: {
+      main: '#ef6c00',
+    },
+  },
+});
+
 class App extends React.PureComponent {
   render() {
     return (
-      <Provider store={store} />
+      <Provider store={store}>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout />
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
